bdc99f57de8a27982240047740ac539e3b9d3b96
om chauhan <omchauhan5020@gmail.com>
2026-04-16 22:44:39 +0530

    adding listings

---
+++ app.js
@@ -0,0 +1,42 @@
+const  express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Listing = require('./models/listing.js');
+
+const mongo_URL = 'mongodb://127.0.0.1:27017/ExploreInn';
+
+main()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+
+async function main() {
+    await mongoose.connect(mongo_URL);
+}
+
+
+app.get('/', (req, res) => {
+    res.send("Hii I am root");
+});
+
+
+app.get("/testListing" , async(req,res) => {
+   let samplelisting = new Listing( {
+    title:"My new Villa",
+    description:"This is a beautiful villa located bank of river.",
+    price: 5000,
+    location: "Rishikesh",
+    country: "India"
+    });
+    await samplelisting.save();
+    console.log("sample listing saved to database");
+    res.send(samplelisting);
+});
+
+app.listen(8000, () => {
+    console.log("Server is running on port 8000");
+});
---
+++ models/listing.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const listingSchema = new schema({
+        title: {
+            type: String,
+            required: true
+        },
+        description: String,
+        Image: {
+            type: String,
+            set: (v) => 
+                v==""
+            ? "https://unsplash.com/photos/a-canal-with-boats-and-trees-in-amsterdam-ZOlCi6m5ZUo"
+            : v ,
+        },
+        price: Number,
+        location: String,
+        country: String,
+});
+
+const listing = mongoose.model('listing', listingSchema);
+module.exports = listing;       
